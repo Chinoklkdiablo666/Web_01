0240e78386e5c3f5e03b29f76733c139b0a40658
kyu <luisglezmo@hotmail.com>
2023-11-16 04:57:28 +0100

    merch final provisional

--- js/merch.js
+++ js/merch.js
@@ -1,24 +1,40 @@
-// seleccionar elementos bootstrap y js
-var comprarButton = document.querySelector('.btn-primary');
-var sizeSelect = document.querySelector('#size');
-var quantityInput = document.querySelector('#quantity');
-var myModal = document.getElementById('myModal');
+// Selecciona los botones "Comprar ahora"
+var comprarButtons = document.querySelectorAll('.btn-primary');
 
-// INICIAR MODAL
+// Selecciona las cajas de entrada de cantidad y el selector de tamaño para el primer producto
+var quantityInputs = [document.querySelector('#quantity'), document.querySelector('#quantity2')];
+var sizeSelect = document.querySelector('#size');
+
+// Inicializa el Modal de Bootstrap
+var myModal = document.getElementById('myModal');
 var modal = new bootstrap.Modal(myModal, {keyboard: false, backdrop: 'static'});
 
-// EVENT LISTENER BOTON
-comprarButton.addEventListener('click', function(event) {
-    if (!sizeSelect.value || !quantityInput.value) {
-        alert('Por favor, selecciona una talla y una cantidad');
-    } else {
-        modal.show();
-    }
+// Agrega un escuchador de eventos a cada botón "Comprar ahora"
+comprarButtons.forEach(function(comprarButton, index) {
+    comprarButton.addEventListener('click', function(event) {
+        // Verifica si la caja de entrada de cantidad correspondiente tiene un valor
+        if (!quantityInputs[index].value) {
+            // Si no tiene un valor, muestra una indicación visual
+            alert('Por favor, selecciona una cantidad');
+        } else if (index === 0 && !sizeSelect.value) {
+            // Si no se ha seleccionado un tamaño para el primer producto, muestra una indicación visual
+            alert('Por favor, selecciona una talla');
+        } else {
+            // Si tiene un valor, muestra el modal
+            modal.show();
+        }
+    });
 });
 
-// INICIAR CARRUSEL
+// Inicializa los Carruseles de Bootstrap
 var myCarousel = document.querySelector('#myCarousel')
 var carousel = new bootstrap.Carousel(myCarousel, {
+  interval: 2000,
+  wrap: true
+})
+
+var myCarousel2 = document.querySelector('#myCarousel2')
+var carousel2 = new bootstrap.Carousel(myCarousel2, {
   interval: 2000,
   wrap: true
 })
